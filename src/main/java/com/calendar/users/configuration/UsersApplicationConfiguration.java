97393739388437d2481fb49ef08498b781039c88
theobanette <theobanette@hotmail.fr>
2025-12-01 14:33:41 +0100

    passage de webmvc à webflux pour le réactif

--- src/main/java/com/calendar/users/configuration/UsersApplicationConfiguration.java
+++ src/main/java/com/calendar/users/configuration/UsersApplicationConfiguration.java
@@ -5,7 +5,10 @@ import com.calendar.users.domain.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
 @Configuration
+@EnableWebFluxSecurity
 public class UsersApplicationConfiguration {
 
     @Bean
