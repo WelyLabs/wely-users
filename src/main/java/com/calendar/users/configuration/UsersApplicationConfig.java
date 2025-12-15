fe9ff4e4bce9fab2bfb825d8a4229597c83e5c42
theobanette <theobanette@hotmail.fr>
2025-12-15 23:13:13 +0100

    rename config + changement de port

--- src/main/java/com/calendar/users/configuration/UsersApplicationConfig.java
+++ src/main/java/com/calendar/users/configuration/UsersApplicationConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UsersApplicationConfiguration {
+public class UsersApplicationConfig {
 
     @Bean
     public UserService userService(
