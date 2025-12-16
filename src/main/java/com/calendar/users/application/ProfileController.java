f9d8f0bcf5b5bdb0f90695117b44307403df3fe5
theobanette <theobanette@hotmail.fr>
2025-12-16 11:42:24 +0100

    ajout d'un controlleur pour les opérations sur le profil d'un utilisateur

---
+++ src/main/java/com/calendar/users/application/ProfileController.java
@@ -0,0 +1,36 @@
+package com.calendar.users.application;
+
+import com.calendar.users.domain.models.BusinessUser;
+import com.calendar.users.domain.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("profile")
+public class ProfileController {
+
+    private final UserService userService;
+
+    public ProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("resolve")
+    public Mono<ResponseEntity<Long>> resolveInternalUserId(@RequestHeader("X-Keycloak-Sub") String keycloakId) {
+        return userService.resolveInternalUserId(keycloakId).map(ResponseEntity::ok);
+    }
+
+    @GetMapping()
+    public Mono<ResponseEntity<BusinessUser>> readProfile(
+            @RequestHeader("X-Internal-User-Id") String userId) {
+        return userService.readProfile(userId).map(ResponseEntity::ok);
+    }
+
+    @PostMapping("profile/picture")
+    public Mono<ResponseEntity<String>> updateProfilePicture(
+            @RequestHeader("X-Keycloak-Sub") String keycloakId, @RequestPart("file") Mono<FilePart> filePartMono) {
+        return userService.updateProfilePicture(keycloakId, filePartMono).map(ResponseEntity::ok);
+    }
+}
--- src/main/java/com/calendar/users/application/UserController.java
+++ src/main/java/com/calendar/users/application/UserController.java
@@ -1,13 +1,7 @@
 package com.calendar.users.application;
 
-import com.calendar.users.domain.models.BusinessUser;
 import com.calendar.users.domain.services.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("users")
@@ -17,23 +11,6 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
-    }
-
-    @GetMapping("me")
-    public Mono<ResponseEntity<BusinessUser>> readProfile(
-            @RequestHeader("X-Internal-User-Id") String userId) {
-        return userService.readProfile(userId).map(ResponseEntity::ok);
-    }
-
-    @GetMapping("resolve")
-    public Mono<ResponseEntity<Long>> resolveInternalUserId(@RequestHeader("X-Keycloak-Sub") String keycloakId) {
-        return userService.resolveInternalUserId(keycloakId).map(ResponseEntity::ok);
-    }
-
-    @PostMapping("me/profilePicture")
-    public Mono<ResponseEntity<String>> updateProfilePicture(
-            @RequestHeader("X-Keycloak-Sub") String keycloakId, @RequestPart("file") Mono<FilePart> filePartMono) {
-        return userService.updateProfilePicture(keycloakId, filePartMono).map(ResponseEntity::ok);
     }
 
 }
