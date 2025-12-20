194497b5412ba6fc88439ef45ac82b4edcc20971
theobanette <theobanette@hotmail.fr>
2025-12-21 00:53:03 +0100

    modifs config kafka

--- src/main/java/com/calendar/users/configuration/KafkaConfig.java
+++ src/main/java/com/calendar/users/configuration/KafkaConfig.java
@@ -1,12 +1,10 @@
 package com.calendar.users.configuration;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -29,8 +27,6 @@ public class KafkaConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        // Sécurité pour Confluent Cloud
         configProps.put("security.protocol", "SASL_SSL");
         configProps.put("sasl.mechanism", "PLAIN");
         configProps.put("sasl.jaas.config", jaasConfig);
